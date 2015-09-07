cf94d10218a80371a33efae23845c71b195ec098
jorgegvalencia <jorgegvalencia@gmail.com>
2015-09-07 22:52:08 +0200

    XMLParsing

--- src/ws/Rest.java
+++ src/ws/Rest.java
@@ -3,51 +3,40 @@ package ws;
 import java.io.*;
 import java.net.*;
 
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
 public class Rest {
 
 	public static void main(String[] args) throws IOException {
 		try {
-
 			URL url = new URL("https://clinicaltrials.gov/show/NCT02087852?displayxml=true");
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
 			conn.setRequestProperty("Accept", "application/xml");
-
 			if (conn.getResponseCode() != 200) {
 				throw new RuntimeException("Failed : HTTP error code : "
 						+ conn.getResponseCode());
 			}
-
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					(conn.getInputStream())));
-			PrintWriter pw = new PrintWriter("C:/Users/Jorge/nlp/input.txt","UTF-8");
-
-			String output;
-			boolean criteria = false;
-			while ((output = br.readLine()) != null) {
-				if(output.contains("<eligibility>")){
-					criteria = true;
-				}
-				else if(output.contains("</eligibility>")){
-					criteria = false;
-				}
-				if(criteria){
-					System.out.println(output);
-					pw.println(output);
-				}
-				pw.close();
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+			
+			XMLInputFactory factory = XMLInputFactory.newInstance();
+			XMLStreamReader streamReader = factory.createXMLStreamReader(br);
+			
+			while(streamReader.hasNext()){
+			    streamReader.next();
+			    if(streamReader.getEventType() == XMLStreamReader.START_ELEMENT){
+			        System.out.println(streamReader.getLocalName());
+			    }
 			}
-
 			conn.disconnect();
-
 		} catch (MalformedURLException e) {
-
 			e.printStackTrace();
-
 		} catch (IOException e) {
-
 			e.printStackTrace();
-
+		} catch (XMLStreamException e) {
+			e.printStackTrace();
 		}
 	}
 }
